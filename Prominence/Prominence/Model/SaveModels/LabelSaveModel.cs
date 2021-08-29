9f81bd2d350d82bb2c3ceed94708055c7ae1fb2a
KingHarley <cweiss1995@gmail.com>
2021-08-29 20:52:06 +0800

    Added SaveModels directory. Created PlayerSaveModel and LabelSaveModel

---
+++ Prominence/Prominence/Model/SaveModels/LabelSaveModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prominence.Model.SaveModels
+{
+    public class LabelSaveModel
+    {
+        public string labelText { get; set; }
+        public string labelType { get; set; }
+    }
+}
---
+++ Prominence/Prominence/Model/SaveModels/PlayerSaveModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Prominence.Model.SaveModels
+{
+    public class PlayerSaveModel
+    {
+        public string Name { get; set; }
+
+        public string Film { get; set; }
+        public string Act { get; set; }
+        public string Scene { get; set; }
+        public string Frame { get; set; }
+        public double Energy { get; set; }
+        public double MaxEnergy { get; set; }
+        public DateTime LastLogin { get; set; }
+        public List<string> Inventory = new List<string>();
+        public List<string> Visited = new List<string>();
+        public List<Label> Log = new List<Label>();
+        public Dictionary<string, int> Flags = new Dictionary<string, int>();
+    }
+}
