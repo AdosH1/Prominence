cc03e5205b731f6d995342af5d1c9e9b06a5670e
Charles Weiss <22159666@student.uwa.edu.au>
2021-08-29 15:19:32 +0800

    B Re-added utilities project. Moved Json serialization to utilities project

--- Prominence/Prominence/Controllers/SaveController.cs
+++ Prominence/Prominence/Controllers/SaveController.cs
@@ -9,34 +9,8 @@ using Prominence.Model.Interfaces;
 
 namespace Prominence.Controllers
 {
-    public class SaveController
+    public static class SaveController
     {
-        public SaveController()
-        {
-           
-        }
-
-        public string JsonSerialize<T>(T itemToSerialize)
-        {
-            return JsonConvert.SerializeObject(itemToSerialize);
-        }
-
-        public T JsonDeserialize<T>(string stringToDeserialize)
-        {
-            T outputItem = JsonConvert.DeserializeObject<T>(stringToDeserialize);
-            return outputItem;
-        }
-
-        public void SaveToAppProperties<T>(string propertiesKey, T objectToSave)
-        {
-            App.Current.Properties[propertiesKey] = JsonSerialize<T>(objectToSave);
-            App.Current.SavePropertiesAsync();
-        }
-
-        public T LoadFromAppProperties<T>(string propertiesKey)
-        {
-            return JsonDeserialize<T>(App.Current.Properties[propertiesKey] as string);
-        }
-
+        
     }
 }
---
+++ Utilities/JsonSerializer.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utilities
+{
+    public static class JsonSerializer
+    {
+        public static string Serialize<T>(T itemToSerialize)
+        {
+            return JsonConvert.SerializeObject(itemToSerialize);
+        }
+
+        public static T Deserialize<T>(string stringToDeserialize)
+        {
+            T outputItem = JsonConvert.DeserializeObject<T>(stringToDeserialize);
+            return outputItem;
+        }
+    }
+}
