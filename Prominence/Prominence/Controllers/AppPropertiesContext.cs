1b9cbd6ceb7cdbda6f778e91ed0886f178fad0bd
KingHarley <cweiss1995@gmail.com>
2021-08-29 21:02:29 +0800

    Added AppPropertiesContext. And implement Save and Load methods

---
+++ Prominence/Prominence/Controllers/AppPropertiesContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prominence.Controllers
+{
+    public static class AppPropertiesContext
+    {
+        const string ACCESSKEY = "UserSettings";
+        public static void Save(string jsonStringToSave)
+        {
+            App.Current.Properties[ACCESSKEY] = jsonStringToSave;
+        }
+
+        public static string Load()
+        {
+            return App.Current.Properties[ACCESSKEY] as string;
+        }
+    }
+}
