49bc1bf07ba290dc7c68bff1f49f5308fcc5a5d2
AdosH1 <aden.huen@gmail.com>
2021-09-04 00:06:56 +0800

    Add backgrounds to project resources

--- Prominence/Prominence/App.xaml.cs
+++ Prominence/Prominence/App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Prominence.View;
 using System;
+using System.Drawing;
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,6 +15,21 @@ namespace Prominence
 
             //MainPage = new NavigationPage(new MainPage());
             MainPage = new DialogueView();
+            var test = GetImageByName("Sequoia.ComputerRoom");
+            MainPage.BackgroundImageSource = test;
+
+        }
+
+        public static ImageSource GetImageByName(string imageName)
+        {
+            System.Reflection.Assembly asm = System.Reflection.Assembly.GetExecutingAssembly();
+            string resourceName = asm.GetName().Name + ".Properties.Resources";
+            var rm = new System.Resources.ResourceManager(resourceName, asm);
+            var tt = (byte[])rm.GetObject(imageName);
+
+            var imSrc = ImageSource.FromStream(() => new MemoryStream(tt));
+
+            return imSrc;
         }
 
         protected override void OnStart()
