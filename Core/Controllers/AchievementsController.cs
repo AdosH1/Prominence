55071f78184be77e561def1ccfc1c055b9eab9c3
AdosH1 <aden.huen@gmail.com>
2021-09-26 16:43:40 +0800

    Add new achievement paradigm (minus controller)

---
+++ Core/Controllers/AchievementsController.cs
@@ -0,0 +1,38 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Core.Controllers
+{
+    public static class AchievementsController
+    {
+
+        public static AchievementsState GetState(AchievementsModel model, PlayerModel player)
+        {
+            var state = new Dictionary<string, AchievementState>();
+            foreach (var achievement in model.Achievements)
+            {
+                state.Add(achievement.Key, achievement.Value.ToState(player));
+            }
+
+            return new AchievementsState(state);
+        }
+
+        public static List<Achievement> CompareState(AchievementsState initial, AchievementsState current)
+        {
+            var newAchievements = new List<Achievement>();
+            foreach (var init in initial.State)
+            {
+                if (init.Value.Completed != current.State[init.Key].Completed)
+                {
+                    newAchievements.Add(init.Value.GetAchievement());
+                }
+            }
+
+            return newAchievements;
+        }
+
+    }
+}
---
+++ Core/Helpers/AchievementHelper.cs
@@ -0,0 +1,22 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Helpers
+{
+    public class AchievementHelper
+    {
+
+        public static Func<PlayerModel, bool> PlayerInventoryCondition(string item)
+        {
+            return new Func<PlayerModel, bool>((PlayerModel player) => { return player.HasItem(item); });
+        }
+
+        public static Func<PlayerModel, bool> PlayerVisitedCondition(string location)
+        {
+            return new Func<PlayerModel, bool>((PlayerModel player) => { return player.HasVisited(location); });
+        }
+
+    }
+}
