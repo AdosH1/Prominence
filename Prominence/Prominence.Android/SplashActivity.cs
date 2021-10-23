488a0fb134b7b8f67da714ca3ff1e7cffe5eb030
AdosH1 <aden.huen@gmail.com>
2021-10-23 09:10:14 +0800

    Native android splash screen

---
+++ Prominence/Prominence.Android/SplashActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prominence.Droid
+{
+    [Activity(
+        Label = "SplashActivity",
+        Theme = "@style/Theme.Splash",
+        MainLauncher = true,
+        NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            System.Threading.Thread.Sleep(3000);
+            StartActivity(typeof(MainActivity));
+            // Create your application here
+        }
+    }
+}
--- Prominence/Prominence.iOS/Main.cs
+++ Prominence/Prominence.iOS/Main.cs
@@ -7,6 +7,8 @@ using UIKit;
 
 namespace Prominence.iOS
 {
+    /// TODO: Implement splash screen
+    /// Follow this (I think it needs a Macbook (xcode)): https://www.c-sharpcorner.com/article/creating-a-splash-screen-in-xamarin-forms/
     public class Application
     {
         // This is the main entry point of the application.
